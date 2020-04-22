703cf94ca450b6d18ba891a8a2faed4936219b00
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-04-22 08:40:04 -0400

    UPDATE

--- R/plan.R
+++ R/plan.R
@@ -27,23 +27,25 @@ source("R/packages.R")  # Load your packages, e.g. library(drake).
 source("R/functions.R") # Define your custom code as a bunch of functions.
 
 # Scraping homepages
-processed_data = read_data(file_in('district-data-to-scrape.csv'))
+# processed_data = read_data(file_in('district-data-to-scrape.csv'))
+# 
+# table_of_output = scrape_and_process_sites(
+#     list("2020-04-19",
+#          processed_data$district,
+#          processed_data$state,
+#          processed_data$nces_id,
+#          processed_data$url))
+# 
+# write_csv(select(table_of_output, -link), "output/2020-04-19/table-of-output-no-links.csv")
+# 
+# table_of_output <- table_of_output %>% 
+#   mutate(link = map(link, ~as.character(.))) %>% 
+#   unnest(link)
+# 
+# write_csv(table_of_output, "output/2020-04-19/table-of-output.csv")
 
-table_of_output = scrape_and_process_sites(
-    list("2020-04-19",
-         processed_data$district,
-         processed_data$state,
-         processed_data$nces_id,
-         processed_data$url))
-
-write_csv(select(table_of_output, -link), "output/2020-04-19/table-of-output-no-links.csv")
-
-table_of_output <- table_of_output %>% 
-  mutate(link = map(link, ~as.character(.))) %>% 
-  unnest(link)
-
-write_csv(table_of_output, "output/2020-04-19/table-of-output.csv")
+table_of_output <- read_csv("output/2020-04-19/table-of-output.csv")
+table_of_output
 
 # Scraping links
-table_of_output <- read_csv("output/2020-04-19/table-of-output.csv")
 scraped_links = proc_links_and_attachments(table_of_output, "2020-04-19")
