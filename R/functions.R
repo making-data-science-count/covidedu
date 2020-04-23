ae20f81b7eb9e267b17f64e35ef53f5760a51842
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-04-22 20:18:41 -0400

    update attachments

--- R/functions.R
+++ R/functions.R
@@ -31,45 +31,45 @@ access_site <- function(my_date, name, state, id, url) {
     
     write_xml(h, file_path)
   }
-
+  
   t <- h %>%
     html_text()
-
+  
   links <- h %>%
     html_nodes("a")
-
+  
   corona <- str_detect(tolower(t), "corona*")
   covid <- str_detect(tolower(t), "covid*")
   closure <- str_detect(tolower(t), "closure")
-
+  
   link_found <- links %>%
     html_text() %>%
     tolower() %>%
     detector() %>%
     any()
-
+  
   link_logical_to_index <- links %>%
     html_text() %>%
     tolower() %>%
     detector()
-
+  
   link_urls <- links %>%
     rvest::html_attr("href")
-
+  
   # adding base
   link_urls <- ifelse(str_detect(link_urls, "http"), link_urls, str_c(url, link_urls))
-
+  
   link_urls <- link_urls[link_logical_to_index]
-
+  
   print(str_c("Processed", base_dir, "-", state, "-", name, "-", id, "; ",
               "found", corona, " ", covid, " ", closure))
-
+  
   tibble(district_name = name, state = state, nces_id = id, url = url,
          corona = corona, covid = covid, closure = closure,
          scraping_failed = FALSE,
          link_found = link_found,
          link = list(link_urls))
-
+  
 }
 
 scrape_and_process_sites <- function(list_of_args) {
@@ -119,7 +119,7 @@ download_link <- function(link, district, state, nces_id, my_date, page_number) 
   tibble(link = link, district = district, state = state, nces_id = nces_id, page_number = page_number, type = "LINK", found = TRUE)
 }
 
-download_attachment <- function(link, district, state, nces_id, my_date) {
+download_attachment <- function(link, district, state, nces_id, page_number, my_date) {
   print(str_c("processing attachment", link, "from ", nces_id))
   file_ext <- tools::file_ext(link)
   base_dir <- str_c("output/", my_date, "/attachments/")
@@ -134,46 +134,56 @@ download_attachment <- function(link, district, state, nces_id, my_date) {
   tibble(link = link, district = district, state = state, nces_id = nces_id, page_number = page_number, type = "ATTACHMENT", found = TRUE)
 }
 
-proc_links_and_attachments <- function(table_of_output, my_date) {
-  
+proc_links_and_attachments <- function(table_of_output, my_date, which_to_scrape = "both") {
   if (!fs::dir_exists(str_c("output/", my_date, "/links"))) fs::dir_create(str_c("output/", my_date, "/links"))
   if (!fs::dir_exists(str_c("output/", my_date, "/attachments"))) fs::dir_create(str_c("output/", my_date, "/attachments"))
   
   # table_of_output <- table_of_output %>% 
   #   unnest(link)
-
+  
   table_of_output$file_ext <- tools::file_ext(table_of_output$link)
   
   attachments <- filter(table_of_output, file_ext %in% c("pdf", ".docx", ".png", ".doc"))
   all_other <- filter(table_of_output, !file_ext %in% c("pdf", ".docx", ".png", ".doc"))
   
-  link_output <- pmap(list(link = all_other$link,
-                           district = all_other$district_name, 
-                           state = all_other$state,
-                           nces_id = all_other$nces_id,
-                           my_date = my_date,
-                           page_number = all_other$page_number),
-                      possibly(download_link,
-                               otherwise = tibble(link = NA, district = NA, state = NA, nces_id = NA, 
-                                                  page_number = NA,
-                                                  type = "LINK", found = FALSE)))
+  if (which_to_scrape == "attachments" | which_to_scrape == "both") {
+    attachment_output <- pmap(list(link = attachments$link,
+                                   district = attachments$district_name, 
+                                   state = attachments$state,
+                                   nces_id = attachments$nces_id,
+                                   my_date = my_date,
+                                   page_number = attachments$page_number),
+                              possibly(download_attachment,
+                                       otherwise = tibble(link = NA, district = NA, state = NA, nces_id = NA, 
+                                                          page_number = NA,
+                                                          type = "ATTACHMENT", found = FALSE)))
+    
+  }
   
-  attachment_output <- pmap(list(link = attachments$link,
-                                 district = attachments$district_name, 
-                                 state = attachments$state,
-                                 nces_id = attachments$nces_id,
-                                 my_date = my_date,
-                                 page_number = attachments$page_number),
-                            possibly(download_attachment,
-                                     otherwise = tibble(link = NA, district = NA, state = NA, nces_id = NA, 
-                                                        page_number = NA,
-                                                        type = "ATTACHMENT", found = FALSE)))
-  
-  output_df <- map_df(link_output, ~.)
+  if (which_to_scrape == "links") {
+    
+    link_output <- pmap(list(link = all_other$link,
+                             district = all_other$district_name, 
+                             state = all_other$state,
+                             nces_id = all_other$nces_id,
+                             my_date = my_date,
+                             page_number = all_other$page_number),
+                        possibly(download_link,
+                                 otherwise = tibble(link = NA, district = NA, state = NA, nces_id = NA, 
+                                                    page_number = NA,
+                                                    type = "LINK", found = FALSE)))
+    output_df <- map_df(link_output, ~.)
+    
+  }
   attachment_df <- map_df(attachment_output, ~.)
   
-  return(list(output_df, attachment_df))
-  
+  if (which_to_scrape == "both") {
+    return(list(output_df, attachment_df))
+  } else if (which_to_scrape == "attachments") {
+    return(attachment_df)
+  } else {
+    return(output_df)
+  }
 }
 # testing
 # proc_links_and_attachments(table_of_output[1:10, ], my_date = Sys.Date())
